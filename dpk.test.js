0e5ea021171f36c3ec2a4d85d5af33e0408127e6
Liu Zhang <rango0986@gmail.com>
2023-01-26 19:36:16 +0000

    [Refactoring] - test: add test cases for normalizePartitionKey function

--- dpk.test.js
+++ dpk.test.js
@@ -1,8 +1,28 @@
-const { deterministicPartitionKey } = require("./dpk");
+const { normalizePartitionKey } = require("./dpk");
 
-describe("deterministicPartitionKey", () => {
+describe("normalizePartitionKey", () => {
   it("Returns the literal '0' when given no input", () => {
-    const trivialKey = deterministicPartitionKey();
+    const trivialKey = normalizePartitionKey();
     expect(trivialKey).toBe("0");
+  });
+
+  it("Should return the literal '0' when given partitionKey '0' in event", () => {
+    const trivialKey = normalizePartitionKey({ partitionKey: '0' });
+    expect(trivialKey).toBe('0');
+  });
+
+  it("Should not return the literal '0' when given partitionKey of not '123456' in event", () => {
+    const trivialKey = normalizePartitionKey({ partitionKey: '1' });
+    expect(trivialKey).not.toBe('0');
+  });
+
+  it("Should not return the literal '0' when given no input partitionKey in event", () => {
+    const trivialKey = normalizePartitionKey({ anyField: '1' });
+    expect(trivialKey).not.toBe('0');
+  });
+
+  it("Should not return the literal '0' when given partitionKey of number in event", () => {
+    const trivialKey = normalizePartitionKey({ anyField: 1 });
+    expect(trivialKey).not.toBe('0');
   });
 });
